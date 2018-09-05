482a735f0aba21e9e4aa3e149123b0d6bd0a768d
xiancf <xiancf@LAPTOP-SNHC6DS3>
2018-09-05 09:53:23 +0800

    23223

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication2.NewFolder1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UserControl con = new WindowsFormsApplication2.NewFolder1.UserControl1();
+        }
+    }
+}
